3ffa031f128666194e1fce8f71474a6b61b052d7
Richard Wallace <rwallace@thewallacepack.net>
2021-09-28 13:48:05 -0700

    add docker-images to ci build

--- ci.nix
+++ ci.nix
@@ -2,6 +2,15 @@ let
   inherit (import ./.) pkgs backend;
 in pkgs.linkFarm "ci" [
   {
+    name = "docker-images";
+    path = pkgs.linkFarm "docker-images"
+      (pkgs.lib.mapAttrsToList
+        (name: path: { inherit path; name = "${name}.tar.gz"; })
+        backend.docker-images
+      );
+  }
+
+  {
     name = "ci-tools";
     path = pkgs.symlinkJoin {
       name = "ci-tools";
