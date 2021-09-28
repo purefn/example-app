512f728380f8f4d0ee047b40a3de0c4262548e9f
Richard Wallace <rwallace@thewallacepack.net>
2021-09-28 13:31:28 -0700

    fix secret

--- backend/tf/outputs.tf
+++ backend/tf/outputs.tf
@@ -1,7 +1,7 @@
-output "alb_dns_name" {
-  value = module.alb.dns_name
-}
+/* output "alb_dns_name" { */
+/*   value = module.alb.dns_name */
+/* } */
 
-output "ecr_repository_url" {
-  value = module.ecs.repository_url
-}
+/* output "ecr_repository_url" { */
+/*   value = module.ecs.repository_url */
+/* } */
